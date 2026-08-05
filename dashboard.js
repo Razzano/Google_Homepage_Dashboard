15dc967bf56175e81eb707ae9208ac443e0b14b2
Sonny <119897675+Razzano@users.noreply.github.com>
2026-08-05 05:21:57 -0400

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -44,7 +44,7 @@
   // GLOBAL CONSTANTS
   // ===========================================================================
 
-  const TEST_LOCALE = 'es-MX'; // Test LANG_LONG i.e. 'en-US'. Set to null for normal browser detection.
+  const TEST_LOCALE = null; // Test LANG_LONG i.e. 'en-US'. Set to null for normal browser detection.
   const LOCALE = TEST_LOCALE ?? Intl.DateTimeFormat().resolvedOptions().locale;
 
   const BASE_SIZE = 360;
