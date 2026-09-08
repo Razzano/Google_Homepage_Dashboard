16c33e98d19fb8986db5f1a168da9b8d54a845ee
Sonny <119897675+Razzano@users.noreply.github.com>
2026-09-07 20:27:33 -0400

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -20,8 +20,8 @@
 //     Installed Userscripts, and scroll down to Editor and uncheck Word wrap checkbox.
 // NOTE 2:
 //   • To open all Google App Links in new tabs, download Tampermonkey script:
-//     https://github.com/Razzano/Google_App_Links/blob/main/Open_in_New_Tab.js or 
-//     https://raw.githubusercontent.com/Razzano/Google_App_Links/main/Open_in_New_Tab.js
+//     view code @ https://github.com/Razzano/Google_App_Links/blob/main/Open_in_New_Tab.js or 
+//     install from https://raw.githubusercontent.com/Razzano/Google_App_Links/main/Open_in_New_Tab.js
 // APPENDIX:
 //   • GLOBAL CONSTANTS
 //   • DOM HELPERS
